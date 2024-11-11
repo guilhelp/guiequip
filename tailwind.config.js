5905ac781c5c660e0cfdb208f94fa0a7c4d45df6
guilhelp <guilhermelimapaula.dev@gmail.com>
2024-11-11 00:51:33 -0300

    feat: front-end pages

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['JetBrains Mono', 'sans-serif'], // Defina a fonte padrão
+      },
+    },
+  },
+  plugins: [],
+}
+
